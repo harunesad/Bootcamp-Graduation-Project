37584126090760c4fe3b244cb1adbeb780660109
Bekir Evrim Sümer <bekirevrimsumer@gmail.com>
2022-07-29 22:48:47 +0300

    GenericSingletonClass oluşturuldu.

---
+++ Assets/Scripts/Singleton/GenericSingleton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSingleton<T> : MonoBehaviour where T : GenericSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                    instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            }
+            return instance;
+        }
+    }
+}
