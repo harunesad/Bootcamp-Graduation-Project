4b3c19c6287c063a8a370ca1b97c9f88f74eaa87
Harun Aşık <harun.esad@gmail.com>
2022-08-03 18:51:44 +0300

    commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StatePattern
+{
+    public class Player : MonoBehaviour
+    {
+        protected Transform playerSol;
+
+        //The different states the player can be in
+        protected enum PlayerState
+        {
+            Idle,
+            Attack,
+            Die,
+            MoveTowardsEnemy,
+            Lock
+        }
+        //Update the player by giving it a new state
+        public virtual void UpdatePlayer(Transform enemySol)
+        {
+
+        }
+        //Do something based on a state
+        protected void DoAction(Transform enemySol, PlayerState playerMode)
+        {
+            float attackSpeed = 5f;
+
+            switch (playerMode)
+            {
+                case PlayerState.Idle:
+                    //Idle player
+                    break;
+                case PlayerState.Attack:
+                    //Attack enemy
+                    break;
+                case PlayerState.Die:
+                    //Die player
+                    break;
+                case PlayerState.MoveTowardsEnemy:
+                    //Look at the enemy
+                    playerSol.rotation = Quaternion.LookRotation(enemySol.position - playerSol.position);
+                    //Move
+                    playerSol.Translate(playerSol.forward * attackSpeed * Time.deltaTime);
+                    break;
+                case PlayerState.Lock:
+                    //Lock the enemy
+                    break;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Player/RangePlayer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StatePattern
+{
+    public class RangePlayer : Player
+    {
+        PlayerState RangePlayerMode = PlayerState.Idle;
+
+        float health = 100f;
+
+
+        public RangePlayer(Transform rangePlayerSol)
+        {
+            base.playerSol = rangePlayerSol;
+        }
+
+
+        //Update the player enemy's state
+        public override void UpdatePlayer(Transform enemySol)
+        {
+            //The distance between the range player and the enemy
+            float distance = (base.playerSol.position - enemySol.position).magnitude;
+
+            switch (RangePlayerMode)
+            {
+                case PlayerState.Idle:
+                    RangePlayerMode = PlayerState.Lock;
+                    break;
+                case PlayerState.Lock:
+                    RangePlayerMode = PlayerState.Attack;
+                    break;
+                case PlayerState.Attack:
+                    if (health == 0)
+                    {
+                        RangePlayerMode = PlayerState.Die;
+                    }
+                    break;
+            }
+            DoAction(enemySol, RangePlayerMode);
+        }
+    }
+}
